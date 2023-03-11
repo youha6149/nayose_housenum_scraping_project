af5bd6a1d6bae4846ed2aa6d7a5a549950a624b6
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-11 17:51:38 +0900

    Add function to convert time_walk to Suumo format

--- src/process/suumo/conditions.py
+++ src/process/suumo/conditions.py
@@ -5,9 +5,9 @@ from dataclasses import asdict, dataclass
 class SuumoConditions:
     property_name: str = ""
     prefecture: str = ""
-    time_walk: str = ""
+    time_walk: str | int = ""
     year_passed: str = ""
-    property_kind: str = ""
+    property_kind: str = "賃貸"
 
     def to_dict(self):
         return asdict(self)
@@ -19,3 +19,23 @@ class SuumoConditions:
             "year_passed": {"tag": "label", "to_input": "/input"},
             "property_kind": {"tag": "text()", "to_input": "/input"},
         }
+
+    def trans_raw_time_walk(self):
+        w_t = self.time_walk
+        if type(w_t) == "str":
+            return
+
+        if w_t <= 1:
+            self.time_walk = "1分以内"
+        elif w_t <= 3:
+            self.time_walk = "3分以内"
+        elif w_t <= 5:
+            self.time_walk = "5分以内"
+        elif w_t <= 7:
+            self.time_walk = "7分以内"
+        elif w_t <= 10:
+            self.time_walk = "10分以内"
+        elif w_t <= 15:
+            self.time_walk = "15分以内"
+        else:
+            self.time_walk = ""
