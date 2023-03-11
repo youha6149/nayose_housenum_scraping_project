8728bde66e32ddf47d53f7e312feb330f7889422
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-11 14:26:15 +0900

    Create data class for conditions

---
+++ src/process/suumo/conditions.py
@@ -0,0 +1,20 @@
+from dataclasses import asdict, dataclass
+
+
+@dataclass
+class SuumoConditions:
+    prefecture: str = ""
+    time_walk: str = ""
+    year_passed: str = ""
+    property_kind: str = ""
+
+    def to_dict(self):
+        return asdict(self)
+
+    def linked_element_dict(self):
+        return {
+            "prefecture": {"tag": "a", "to_input": "/div/input"},
+            "time_walk": {"tag": "label", "to_input": "/input"},
+            "year_passed": {"tag": "label", "to_input": "/input"},
+            "property_kind": {"tag": "text()", "to_input": "/input"},
+        }
