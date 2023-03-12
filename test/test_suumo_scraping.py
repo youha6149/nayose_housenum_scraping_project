294b57599ed1bf872e6f8c0f91c456e36e8ddf5e
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-12 12:29:17 +0900

    Added loop and error handling

--- test/test_suumo_scraping.py
+++ test/test_suumo_scraping.py
@@ -1,13 +1,26 @@
 import pdb
 
+from log.logger import setup_logger
 from process.suumo.conditions import SuumoConditions
 from process.suumo.scraper import SuumoScraper
 
 
 def test_suumo_scraping():
-    conditions = SuumoConditions(property_name="アルス品川", prefecture="東京都")
+    property_names = ["アルス品川", "ライオンズマンション"]
+    # 以下をまとめて関数として定義する
     with SuumoScraper() as bot:
-        bot.scrape_suumo(conditions=conditions)
+        pdb.set_trace()
+        for name in property_names:
+            conditions = SuumoConditions(property_name=name, prefecture="東京都")
+            if not conditions.time_walk == "":
+                conditions.trans_raw_time_walk()
+            try:
+                bot.scrape_suumo(conditions=conditions)
+            except Exception as e:
+                logger = setup_logger("Scraper_logger", "scraper_error.log")
+                logger.error(f"An error occurred: {e}")
+                continue
+
         print(bot.row_data)
 
 
