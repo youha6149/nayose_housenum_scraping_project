6b5cf6c9494376261f7fc7f2fa276d3e9a5eb9d7
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-12 12:24:27 +0900

    Added error handling

--- src/process/common/scraper.py
+++ src/process/common/scraper.py
@@ -1,12 +1,15 @@
+import json
 import pdb
 import time
 import traceback
 
-from bs4 import BeautifulSoup
+import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+
+from log.logger import setup_logger
 
 
 class Scraper(webdriver.Chrome):
@@ -24,8 +27,18 @@ class Scraper(webdriver.Chrome):
         self.implicitly_wait(15)
         self.set_script_timeout(5)
         self.waitng = WebDriverWait(self, 15)
+        self.error_elements_exc: list[dict[str, str]] = []
+        self.media_dict = {"media": type(self).__name__}
 
     def __exit__(self, exc_type, exc, traceback):
+        if self.error_elements_exc:
+            df = pd.DataFrame(self.error_elements_exc)
+            df.to_csv("element_exc.csv")
+
+        if exc:
+            logger = setup_logger("Scraper_logger", "scraper_error.log")
+            logger.error(f"An error occurred: {exc}")
+
         self.quit()
 
     def page_is_loaded(self):
@@ -36,3 +49,14 @@ class Scraper(webdriver.Chrome):
     def open_page(self, url):
         self.get(url)
         self.waitng.until(lambda x: self.page_is_loaded())
+
+    def __add_element_error_detail(self, e: NoSuchElementException):
+        tb = traceback.extract_tb(e.__traceback__)
+        error_dict = {
+            "media": type(self).__name__,
+            **json.loads(e.msg),
+            "file_name": tb[-1].filename,
+            "error_location": tb[-1].lineno,
+            "function_name": tb[-1].name,
+        }
+        self.error_elements_exc.append(error_dict)
