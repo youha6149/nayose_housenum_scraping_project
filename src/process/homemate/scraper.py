1ee389ebe029731304961a9879fb4359704f575a
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-17 21:16:33 +0900

    Correction of property information acquisition function

--- src/process/homemate/scraper.py
+++ src/process/homemate/scraper.py
@@ -5,17 +5,17 @@ import traceback
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
 from model.nayose import Nayose
 from process.common.scraper import Scraper
-from process.homemate.conditions import HomemateConditions
 
 
 class HomemateScraper(Scraper):
     def __init__(self, default_dl_path="", is_headless=False):
         super().__init__(default_dl_path, is_headless)
-        self.base_url = "https://www.homemate.co.jp/"
+        self.base_url = "https://www.homemate.co.jp"
         self.liblary_url = "https://www.homemate.co.jp/keyword/"
         self.row_data = []
 
@@ -24,7 +24,6 @@ class HomemateScraper(Scraper):
 
         tmp = soup.find("span", class_="m_prpty_result_head_hit").text
         total_num = int(re.sub(r"[^\d]+", "", tmp))
-        pdb.set_trace()
 
         if total_num == 0:
             return
@@ -36,12 +35,20 @@ class HomemateScraper(Scraper):
             pref = record.prefecture
             if pref not in [option.text for option in select_element.options]:
                 return
+
+            select_element.select_by_visible_text(pref)
+            self.waitng.until(
+                EC.presence_of_element_located((By.ID, "deqwas-collection"))
+            )
+
+            soup = BeautifulSoup(self.page_source, "lxml")
             boxes = soup.find_all("section", class_="m_prpty_box")
 
             if len(boxes) > 25:
                 return
 
         search_address = f"{record.prefecture}{record.city}"
+        pdb.set_trace()
         links = [
             box.select_one(
                 "div.m_prpty_itemlist_wrap > div.m_prpty_itemlist > div:nth-child(1) a.m_prpty_item_linkarea_btn.kpi_click"
@@ -53,25 +60,12 @@ class HomemateScraper(Scraper):
         if links:
             return links
 
-    def __scrape_contents(self):
-        soup = BeautifulSoup(self.page_source, "lxml")
-        row_dict = {"物件名": soup.find("h1").text}
-
-        trs = soup.select("#contents > div > div > div > table > tbody > tr")
-        for tr in trs:
-            # {"住所": "北海道 函館市 湯川町３"}
-            th_texts = [th.text for th in tr.find_all("th")]
-            td_texts = [td.text for td in tr.find_all("td")]
-            zip_texts = zip(th_texts, td_texts)
-            row_dict.update(dict(zip_texts))
-
-        self.row_data.append(row_dict)
-
     def scrape_homemate(self, record: Nayose):
         # 検索ボックスから検索
         pdb.set_trace()
         self.open_page(f"{self.liblary_url}{record.name}/")
-        # wait処理をちゃんと実装した方がいいかもしれない
+        self.waitng.until(EC.presence_of_element_located((By.ID, "deqwas-collection")))
+
         # 表示された情報を取得する
         links = self.__get_table_links(record)
 
