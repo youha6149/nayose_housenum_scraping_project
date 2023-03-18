a5c06fbff5c5d1f4158b466902fe52a9c9532919
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-18 15:03:10 +0900

    Creating a scraping class process

--- src/process/homemate/scraper.py
+++ src/process/homemate/scraper.py
@@ -11,6 +11,8 @@ from selenium.webdriver.support.ui import Select
 from model.nayose import Nayose
 from process.common.scraper import Scraper
 
+# TODO:wait処理をcss_selectorとしてcommonクラスにまとめる
+
 
 class HomemateScraper(Scraper):
     def __init__(self, default_dl_path="", is_headless=False):
@@ -30,6 +32,7 @@ class HomemateScraper(Scraper):
 
         boxes = soup.find_all("section", class_="m_prpty_box")
 
+        # 出力されたデータの件数が多い場合、地域の選択から絞り込みを行う
         if len(boxes) > 5:
             select_element = Select(self.find_element(By.ID, "list-city-select2"))
             pref = record.prefecture
@@ -41,14 +44,15 @@ class HomemateScraper(Scraper):
                 EC.presence_of_element_located((By.ID, "deqwas-collection"))
             )
 
+            # 変化したHTMLを再度取得する
             soup = BeautifulSoup(self.page_source, "lxml")
             boxes = soup.find_all("section", class_="m_prpty_box")
-
+            # 絞り込んでもデータ数が多い場合、ブランド名や地域名などのエラーデータの可能性あり
             if len(boxes) > 25:
                 return
 
         search_address = f"{record.prefecture}{record.city}"
-        pdb.set_trace()
+
         links = [
             box.select_one(
                 "div.m_prpty_itemlist_wrap > div.m_prpty_itemlist > div:nth-child(1) a.m_prpty_item_linkarea_btn.kpi_click"
@@ -60,9 +64,24 @@ class HomemateScraper(Scraper):
         if links:
             return links
 
+    def __scrape_contents(self, record: Nayose):
+        soup = BeautifulSoup(self.page_source, "lxml")
+        # 必要なデータは何か
+        # 物件名・階数・物件種別・所在地・アクセス・構造
+        data_element = [
+            th.find_parent("table") for th in soup.select("table th.m_table_ws")
+        ]
+        property_dict = {
+            re.sub(r"[^\w]+", "", d.find("th").text): re.sub(
+                r"[^\w]+", "", d.find("td").text
+            )
+            for d in data_element
+        }
+        pdb.set_trace()
+        self.row_data.append(property_dict)
+
     def scrape_homemate(self, record: Nayose):
         # 検索ボックスから検索
-        pdb.set_trace()
         self.open_page(f"{self.liblary_url}{record.name}/")
         self.waitng.until(EC.presence_of_element_located((By.ID, "deqwas-collection")))
 
@@ -72,7 +91,10 @@ class HomemateScraper(Scraper):
         # 該当するデータが一つもない場合、returnする
         if not links:
             return "not links"
-
+        pdb.set_trace()
         for link in links:
             self.open_page(url=f"{self.base_url}{link}")
-            self.__scrape_contents()
+            self.waitng.until(
+                EC.presence_of_element_located((By.ID, "deqwas-collection"))
+            )
+            self.__scrape_contents(record)
