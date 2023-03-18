8d712624745c9473d1a05278a3f430d1efab804d
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-18 16:58:13 +0900

    Fix suumo scraping test

--- test/test_suumo_scraping.py
+++ test/test_suumo_scraping.py
@@ -1,32 +1,29 @@
 import pdb
 
 from log.logger import setup_logger
+from model.nayose import Nayose
+from model.setting import get_nayose_db
 from process.suumo.conditions import SuumoConditions
 from process.suumo.scraper import SuumoScraper
 
 
 def test_suumo_scraping():
-    property_names = ["ジーメゾン西大宮", "ＤーＲＯＯＭ西大宮ＰＪ北棟", "エレガンテ"]
-    prefectures = ["埼玉県", "埼玉県", "埼玉県"]
-    cities = ["さいたま市西区", "さいたま市西区", "さいたま市西区"]
+    db = get_nayose_db()
+    session = next(db)
+    housenum0_record = session.query(Nayose).filter_by(housenum=0).all()
 
     # 以下をまとめて関数として定義する
     with SuumoScraper() as bot:
-        pdb.set_trace()
-        for i in range(len(property_names)):
-            conditions = SuumoConditions(
-                property_name=property_names[i],
-                prefecture=prefectures[i],
-                city=cities[i],
-            )
-            if not conditions.time_walk == "":
-                conditions.trans_raw_time_walk()
-            try:
-                bot.scrape_suumo(conditions=conditions)
-            except Exception as e:
-                logger = setup_logger("Scraper_logger", "scraper_error.log")
-                logger.error(f"An error occurred: {e}")
-                continue
+        try:
+            for i in range(10):
+                print(i)
+                record = housenum0_record[i]
+                bot.scrape_suumo(record)
+                print(bot.row_data)
+        except Exception as e:
+            logger = setup_logger("Scraper_logger", "scraper_error.log")
+            logger.error(f"An error occurred: {e}")
+            pdb.set_trace()
 
         print(bot.row_data)
 
