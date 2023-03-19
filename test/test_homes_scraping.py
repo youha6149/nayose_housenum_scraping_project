be8b2091cdf4b39ffaed37decd7fcfa83b332fef
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-19 13:03:24 +0900

    Fixed to not block processing

--- test/test_homes_scraping.py
+++ test/test_homes_scraping.py
@@ -1,9 +1,9 @@
 import pdb
+import time
 
 from log.logger import setup_logger
 from model.nayose import Nayose
 from model.setting import get_nayose_db
-from process.homes.conditions import HomesConditions
 from process.homes.scraper import HomesScraper
 
 
@@ -12,36 +12,36 @@ def test_suumo_scraping():
     session = next(db)
     housenum0_record = session.query(Nayose).filter_by(housenum=0).all()
 
-    # 以下をまとめて関数として定義する
-    with HomesScraper() as bot:
-        for i in range(10):
-            pdb.set_trace()
-            record = housenum0_record[i]
-            conditions = HomesConditions(
-                name=record.name,
-                prefecture=record.prefecture,
-                city=record.city,
-                town=record.town,
-                time_walk=record.timewalk,
-            )
-            bot.scrape_homes(conditions=conditions)
+    all_data = []
+    for i in range(48, 58):
+        try:
+            with HomesScraper() as bot:
+                # 物件一覧ページに遷移
+                print(i)
+                bot.open_page(f"{bot.liblary_url}{housenum0_record[i].name}")
+                time.sleep(1)
+                # 物件一覧から各物件のURLを取得する
+                links = bot.get_table_links(housenum0_record[i])
 
-        # for i in range(len(property_names)):
-        # conditions = SuumoConditions(
-        #     property_name=property_names[i],
-        #     prefecture=prefectures[i],
-        #     city=cities[i],
-        # )
-        # if not conditions.time_walk == "":
-        #     conditions.trans_raw_time_walk()
-        # try:
-        #     bot.scrape_suumo(conditions=conditions)
-        # except Exception as e:
-        #     logger = setup_logger("Scraper_logger", "scraper_error.log")
-        #     logger.error(f"An error occurred: {e}")
-        #     continue
+            if links is None:
+                continue
 
-        print(bot.row_data)
+            for link in links:
+                with HomesScraper() as bot:
+                    # 物件詳細ページに遷移
+                    bot.open_page(f"{bot.base_url}{link}")
+                    time.sleep(1)
+
+                    bot.scrape_table_data()
+                    all_data.append(bot.row_data)
+
+                time.sleep(2)
+
+        except Exception as e:
+            print(e)
+            continue
+
+    print(all_data)
 
 
 if __name__ == "__main__":
