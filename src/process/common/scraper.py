f2e7a9cf73de443ed0b7ea81d4c19fc33fa4452c
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-20 14:14:16 +0900

    Fixed error handling

--- src/process/common/scraper.py
+++ src/process/common/scraper.py
@@ -1,9 +1,7 @@
-import json
 import pdb
 import time
 import traceback
 
-import pandas as pd
 from bs4 import BeautifulSoup, NavigableString, ResultSet, Tag
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -11,8 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
-from log.logger import setup_logger
 
 
 class Scraper(webdriver.Chrome):
@@ -30,19 +26,9 @@ class Scraper(webdriver.Chrome):
         self.implicitly_wait(15)
         self.set_script_timeout(5)
         self.waitng = WebDriverWait(self, 15)
-        self.error_elements_exc: list[dict[str, str]] = []
-        self.media_dict = {"media": type(self).__name__}
         self.soup = ""
 
     def __exit__(self, exc_type, exc, traceback):
-        if self.error_elements_exc:
-            df = pd.DataFrame(self.error_elements_exc)
-            df.to_csv("element_exc.csv")
-
-        if exc:
-            logger = setup_logger("Scraper_logger", "scraper_error.log")
-            logger.error(f"An error occurred: {exc}")
-
         self.quit()
 
     def page_is_loaded(self):
@@ -53,58 +39,54 @@ class Scraper(webdriver.Chrome):
     def open_page(self, url):
         self.get(url)
         self.waitng.until(lambda x: self.page_is_loaded())
-
-    def __add_element_error_detail(self, e: NoSuchElementException):
-        tb = traceback.extract_tb(e.__traceback__)
-        error_dict = {
-            "media": type(self).__name__,
-            **json.loads(e.msg),
-            "file_name": tb[-1].filename,
-            "error_location": tb[-1].lineno,
-            "function_name": tb[-1].name,
-        }
-        self.error_elements_exc.append(error_dict)
+        self.init_soup()
 
     def wait_presence_of_element_by_cssselector(self, selector: str, waittime: int):
         self.waitng.until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
         time.sleep(waittime)
 
-    def _init_src_element(self, src: Tag = "") -> Tag | BeautifulSoup:
+    def init_soup(self):
+        """ページ全体のBeautifulSoupを初期化する(DOM変化のたびに実行)"""
+        self.soup = BeautifulSoup(self.page_source, "lxml")
+
+    def init_src_element(self, src: Tag = "") -> Tag | BeautifulSoup:
         if not src == "":
             return src
-
-        self.soup = BeautifulSoup(self.page_source, "lxml")
 
         return self.soup
 
     def get_element_by_find(self, tag, src="", **attr) -> Tag | NavigableString:
         """.findを用いて要素取得を行う。要素がない場合NoSuchElementExceptionを返す"""
-        src_elm = self._init_src_element(src)
+        src_elm = self.init_src_element(src)
         element = src_elm.find(tag, **attr)
         if element is None:
-            raise NoSuchElementException
+            raise NoSuchElementException(f"Element not found: {tag} {attr}")
+
         return element
 
     def get_elements_by_find_all(self, tag, src="", **attr) -> ResultSet:
         """.find_allを用いて複数の要素取得を行う。要素がない場合NoSuchElementExceptionを返す"""
-        src_elm = self._init_src_element(src)
+        src_elm = self.init_src_element(src)
         elements = src_elm.find_all(tag, **attr)
         if elements is None:
-            raise NoSuchElementException
+            raise NoSuchElementException(f"Element not found: {tag} {attr}")
+
         return elements
 
     def get_element_by_select_one(self, selector, src="") -> Tag:
         """.select_oneを用いて要素取得を行う。要素がない場合NoSuchElementExceptionを返す"""
-        src_elm = self._init_src_element(src)
-        elements = src_elm.select_one(selector)
-        if elements is None:
-            raise NoSuchElementException
-        return elements
+        src_elm = self.init_src_element(src)
+        element = src_elm.select_one(selector)
+        if element is None:
+            raise NoSuchElementException(f"Element not found: {selector}")
+
+        return element
 
     def get_elements_by_select(self, selector, src="") -> ResultSet[Tag]:
         """.selectを用いて複数の要素取得を行う。要素がない場合NoSuchElementExceptionを返す"""
-        src_elm = self._init_src_element(src)
+        src_elm = self.init_src_element(src)
         elements = src_elm.select(selector)
         if elements is None:
-            raise NoSuchElementException
+            raise NoSuchElementException(f"Element not found: {selector}")
+
         return elements
