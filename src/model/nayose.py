af453e34a42488fdeb85d2f5f2a6018d533704f1
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-21 15:13:03 +0900

    Added a function to delete the CSV file used at the end of _init_data processing.

--- src/model/nayose.py
+++ src/model/nayose.py
@@ -3,10 +3,10 @@ import os
 import pandas as pd
 from sqlalchemy import Column, Integer, String
 
-if not __name__ == "__main__":
-    from model.setting import Base, nayose_engine
-else:
+if __name__ == "__main__":
     from setting import Base, nayose_engine
+else:
+    from model.setting import Base, nayose_engine
 
 
 class Nayose(Base):
@@ -26,11 +26,15 @@ class Nayose(Base):
     def _init_data(cls, csv_file_path: str):
         nayose_df = pd.read_csv(csv_file_path)
         nayose_df["id"] = nayose_df.index
-        nayose_trans_df = nayose_df[[k for k in vars(cls) if not k.startswith("_")]]
+        nayose_trans_df = nayose_df[
+            [k for k in vars(cls) if not k.startswith("_") and not k == "to_dict"]
+        ]
 
         nayose_trans_df.to_sql(
             cls.__tablename__, nayose_engine, index=False, if_exists="replace"
         )
+
+        os.remove(csv_file_path)
 
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
