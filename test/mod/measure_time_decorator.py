9b3165cae281d0e4f6235ebb6ea066529cdfd70f
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-25 17:36:14 +0900

    Added file output function to test instrumentation decorator

--- test/mod/measure_time_decorator.py
+++ test/mod/measure_time_decorator.py
@@ -1,3 +1,4 @@
+import csv
 import time
 
 
@@ -8,6 +9,11 @@ def measure_time(func):
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"{func.__name__} の処理時間は {elapsed_time:.5f} 秒です")
+
+        with open("function_times.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([func.__name__, elapsed_time])
+
         return result
 
     return wrapper
