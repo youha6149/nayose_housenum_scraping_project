5b03f5a4c61461f0049a2f4dd6ef37465a9b2e50
kakeruuu <kakeruuuuuu0114@gmail.com>
2023-03-30 11:16:33 +0900

    Create a test when the scraping process is acquired via __init__

---
+++ test/test_run_method.py
@@ -0,0 +1,33 @@
+import os
+import pdb
+
+import pandas as pd
+
+import process
+from model.nayose import Nayose
+from model.setting import get_nayose_db
+from process.common.utils import Util
+
+
+def test_run_method():
+    """process.__init__で設定したスクレイピング実行モジュール群を呼び出してスクレイピングを実行するテスト"""
+    db = get_nayose_db()
+    session = next(db)
+    housenum0record = session.query(Nayose).filter_by(housenum=0).all()
+    utils = Util()
+    merge_dfs_list = []
+    pdb.set_trace()
+
+    for run_scraper in process.run_functions:
+        output_data = run_scraper(housenum0record)
+        data = list(output_data.values())[0]
+        if data and not output_data == None:
+            df = utils.trans_cols_name(output_data)
+            merge_dfs_list.append(df)
+
+    merge_df = pd.concat(merge_dfs_list, axis=0)
+    merge_df.to_csv("test_scraping.csv", encoding="utf-8")
+
+
+if __name__ == "__main__":
+    test_run_method()
